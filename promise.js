b7ef71a5455dd3b2b1c556b7d4ab6e637995c4e0
Mark24 <mark.zhangyoung@qq.com>
2021-01-13 15:26:30 +0800

    补齐resolve，reject

--- promise.js
+++ promise.js
@@ -1,4 +1,15 @@
 function Promise(executor) {
+
+    //resolve
+    function resolve(data) {
+        // data就是会传参数
+    }
+
+    // reject
+    function reject(data) {
+        
+    }
+
     // executor 是同步执行的,直接执行就好
     executor(resolve, reject);
     
