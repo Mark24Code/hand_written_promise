caf140e31ed04dfb8fce5aec128363c2a0cd657d
Mark24 <mark.zhangyoung@qq.com>
2021-01-13 16:04:30 +0800

    then支持异步任务

--- promise.js
+++ promise.js
@@ -4,6 +4,9 @@ function Promise(executor) {
     this.PromiseState = 'pending';
     this.PromiseResult = null;
 
+    this.callback = {};
+
+
     // 保存this值指向实例，保留给resolve，reject
     const self = this;
 
@@ -12,9 +15,15 @@ function Promise(executor) {
         // 1.修改对象状态 promiseState
         // 2.修改对象结果值 promiseResult
 
-        if (self.PromiseState === 'pending') {
-            self.PromiseState = 'fulfilled';
-            self.PromiseResult = data;
+        if(self.PromiseState !== 'pending') {
+            return
+        }
+
+        self.PromiseState = 'fulfilled';
+        self.PromiseResult = data;
+        // 应该去调用 then
+        if(self.callback.onResolved) {
+            self.callback.onResolved(data)
         }
     }
 
@@ -22,11 +31,16 @@ function Promise(executor) {
     function reject(data) {
         // 1.修改对象状态 promiseState
         // 2.修改对象结果值 promiseResult
-        if(self.PromiseState === 'pending') {
-            self.PromiseState = 'rejected';
-            self.PromiseResult = data;
+        if (self.PromiseState !== 'pending') {
+            return
         }
 
+        self.PromiseState = 'rejected';
+        self.PromiseResult = data;
+        // 应该去调用 then
+        if (self.callback.onRejected) {
+            self.callback.onRejected(data)
+        }
     }
 
     try {
@@ -43,15 +57,27 @@ function Promise(executor) {
 // 不同的状态对应不同的回调函数
 // 调用then的是实例，所以可以用this
 // 并且可以传入回调的值
-Promise.prototype.then = function (onResolve, onRejected) {
+Promise.prototype.then = function (onResolved, onRejected) {
     if(this.PromiseState === 'fulfilled') {
-        onResolve(this.PromiseResult)
+        onResolved(this.PromiseResult)
     } 
 
     if(this.PromiseState === 'rejected') {
         onrejectionhandled(this.PromiseResult)
     }
     
+    // 异步
+    if (this.PromiseState === 'pending') {
+        // 应该是pending的时候就不调用
+        // 改变状态的时候就去调用
+        // 改变状态的 onResolved、onRejected后面又如何调用then呢？
+        // 为了让他们可以调用then
+        // 要保存回调函数
+        this.callback = {
+            onResolved: onResolved,
+            onRejected: onRejected
+        }
+    }
 }
 
 
