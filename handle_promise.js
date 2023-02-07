7498e521846d86f814691cdea5c0ce229b4274af
Mark24 <mark.zhangyoung@gmail.com>
2023-02-07 17:30:32 +0800

    step5: 修正then，新增reoslve，reject 修正promiseA+报错

--- handle_promise.js
+++ handle_promise.js
@@ -49,6 +49,11 @@ class Promise {
 
 
   then(onFulfilled, onRejected) {
+
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+    onRejected = typeof onRejected === 'function' ? onRejected : reason => {throw reason};
+
+
     const promise2 = new Promise((resolve, reject) => {
       if(this.status === FULFILLED) {
         queueMicrotask(() => {
@@ -95,23 +100,100 @@ class Promise {
 
     return promise2
   }
+
+
+  // resolve 静态方法
+  static resolve (parameter) {
+    // 如果传入 MyPromise 就直接返回
+    if (parameter instanceof Promise) {
+      return parameter;
+    }
+
+    // 转成常规方式
+    return new Promise(resolve =>  {
+      resolve(parameter);
+    });
+  }
+
+  // reject 静态方法
+  static reject (reason) {
+    return new Promise((resolve, reject) => {
+      reject(reason);
+    });
+  }
+
 }
 
-function resolvePromise(promise2, x, resolve, reject) {
-  if(promise2 === x) {
-    return reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
+function resolvePromise(promise, x, resolve, reject) {
+  // 如果相等了，说明return的是自己，抛出类型错误并返回
+  if (promise === x) {
+    return reject(new TypeError('The promise and the return value are the same'));
   }
 
-  // 判断x是不是 Promise 实例对象
-  if(x instanceof Promise) {
-    // 执行 x，调用 then 方法，目的是将其状态变为 fulfilled 或者 rejected
-    // x.then(value => resolve(value), reason => reject(reason))
-    // 简化之后
-    x.then(resolve, reject)
-  } else{
-    // 普通值
-    resolve(x)
+  if (typeof x === 'object' || typeof x === 'function') {
+    // x 为 null 直接返回，走后面的逻辑会报错
+    if (x === null) {
+      return resolve(x);
+    }
+
+    let then;
+    try {
+      // 把 x.then 赋值给 then
+      then = x.then;
+    } catch (error) {
+      // 如果取 x.then 的值时抛出错误 error ，则以 error 为据因拒绝 promise
+      return reject(error);
+    }
+
+    // 如果 then 是函数
+    if (typeof then === 'function') {
+      let called = false;
+      try {
+        then.call(
+          x, // this 指向 x
+          // 如果 resolvePromise 以值 y 为参数被调用，则运行 [[Resolve]](promise, y)
+          y => {
+            // 如果 resolvePromise 和 rejectPromise 均被调用，
+            // 或者被同一参数调用了多次，则优先采用首次调用并忽略剩下的调用
+            // 实现这条需要前面加一个变量 called
+            if (called) return;
+            called = true;
+            resolvePromise(promise, y, resolve, reject);
+          },
+          // 如果 rejectPromise 以据因 r 为参数被调用，则以据因 r 拒绝 promise
+          r => {
+            if (called) return;
+            called = true;
+            reject(r);
+          });
+      } catch (error) {
+        // 如果调用 then 方法抛出了异常 error：
+        // 如果 resolvePromise 或 rejectPromise 已经被调用，直接返回
+        if (called) return;
+
+        // 否则以 error 为据因拒绝 promise
+        reject(error);
+      }
+    } else {
+      // 如果 then 不是函数，以 x 为参数执行 promise
+      resolve(x);
+    }
+  } else {
+    // 如果 x 不为对象或者函数，以 x 为参数执行 promise
+    resolve(x);
   }
+}
+
+
+
+Promise.deferred = function () {
+  var result = {};
+  result.promise = new Promise(function (resolve, reject) {
+    result.resolve = resolve;
+    result.reject = reject;
+  });
+
+  return result;
 }
 
 module.exports = Promise
